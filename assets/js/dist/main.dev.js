0c2914e731b40a74079784863addfcee3ce0f5d0
jovsz <jovannyfuentesm@gmail.com>
2021-01-13 15:03:31 -0800

    first commit

---
+++ assets/js/dist/main.dev.js
@@ -0,0 +1,100 @@
+"use strict";
+
+var questions = [];
+var score = 0;
+
+function getCategory() {
+  var url = 'https://opentdb.com/api_category.php';
+  fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (categoryData) {
+    return printCategory(categoryData.trivia_categories);
+  });
+}
+
+function getQuestions() {
+  var bool = false;
+  var totalQuestions = document.getElementById('totalQuestions').value;
+  var category = document.getElementById('category').value;
+  var difficult = document.getElementById('difficult').value;
+
+  if (category == '') {
+    alert('Please select a Categorie');
+    return bool = true;
+  }
+
+  if (difficult == '') {
+    alert('Please select a Difficult');
+    return bool = true;
+  }
+
+  if (totalQuestions == '') {
+    alert('Please select an amount of questions');
+    return bool = true;
+  }
+
+  if (bool === false) {
+    //alert('traemos las preguntas de la api');
+    var url = "https://opentdb.com/api.php?amount=".concat(totalQuestions, "&category=").concat(category, "&difficulty=").concat(difficult);
+    fetch(url) //devolver la respues en formato json
+    .then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      return printData(data.results);
+    });
+  }
+
+  console.log(bool);
+}
+
+function printCategory(categoryData) {
+  var container = document.getElementById('category');
+  categoryData.forEach(function (element) {
+    container.innerHTML += "<option value=\"".concat(element.id, "\">").concat(element.name, "</option>");
+  });
+}
+
+function printData(data) {
+  var container = document.getElementById('questions-container');
+  var html = '';
+
+  for (var i = 0; i < data.length; i++) {
+    questions.push({
+      question: data[i].question,
+      answer: data[i].correct_answer
+    });
+    data[i].incorrect_answers.push(data[i].correct_answer);
+
+    if (data[i].incorrect_answers.length > 2) {
+      html += "<h5 id=\"question\"}\">".concat(i + 1, ".-").concat(data[i].question, "</h5>\n                <select id=\"answer-").concat(i, "\"  class=\"form-select\" aria-label=\"Default select example\">\n                    <option selected value=\"\"></option>\n                    <option value= \"").concat(data[i].incorrect_answers[0], "\">").concat(data[i].incorrect_answers[0], "</option>\n                    <option value= \"").concat(data[i].incorrect_answers[1], "\">").concat(data[i].incorrect_answers[1], "</option>\n                    <option value= \"").concat(data[i].incorrect_answers[2], "\">").concat(data[i].incorrect_answers[2], "</option>\n                    <option value= \"").concat(data[i].incorrect_answers[3], "\">").concat(data[i].incorrect_answers[3], "</option>\n                </select>\n                ");
+    } else {
+      html += "<h5 id=\"question\"}\">".concat(i + 1, ".-").concat(data[i].question, "</h5>\n                <select id=\"answer-").concat(i, "\" class=\"form-select\" aria-label=\"Default select example\">\n                    <option selected value=\"\"></option>\n                    <option value= \"").concat(data[i].incorrect_answers[0], "\">").concat(data[i].incorrect_answers[0], "</option>\n                    <option value= \"").concat(data[i].incorrect_answers[1], "\">").concat(data[i].incorrect_answers[1], "</option>\n                </select>");
+    }
+  }
+
+  html += "<button onclick=\"checkResult()\" type=\"submit\" class=\"btn btn-primary\">Send Answers</button>";
+  container.innerHTML = html;
+}
+
+function checkResult() {
+  var score = 0;
+  var container = document.getElementById('finalScore');
+
+  for (var i = 0; i < questions.length; i++) {
+    var result = document.getElementById("answer-".concat(i)).value;
+    console.log(result);
+
+    if (result == '') {
+      alert('Please choose your answer to question: ' + (i + 1));
+      document.getElementById("answer-".concat(i)).focus;
+    } else {
+      if (result === questions[i].answer) {
+        score += 1;
+      }
+    }
+  }
+
+  container.innerHTML = "<div class=\"score\" id=\"finalScore\">\n                                <p class=final>Your Score is ".concat(score, " of ").concat(questions.length, "</p>\n                                <button type=\"submit\" onclick=\"#\" class=\"btn btn-primary\">Try Again</button>\n                           </div>");
+}
+
+getCategory();
---
+++ assets/js/main.js
@@ -0,0 +1,115 @@
+let questions = [];
+
+let score = 0;
+
+function getCategory() {
+    const url = 'https://opentdb.com/api_category.php';
+
+    fetch(url)
+        .then(response => response.json())
+        .then((categoryData) => printCategory(categoryData.trivia_categories));
+}
+
+function getQuestions() {
+    let bool = false;
+    const totalQuestions = document.getElementById('totalQuestions').value;
+    const category = document.getElementById('category').value;
+    const difficult = document.getElementById('difficult').value;
+
+    if (category == '') {
+        alert('Please select a Categorie');
+        return bool = true;
+
+
+    }
+    if (difficult == '') {
+        alert('Please select a Difficult');
+        return bool = true;
+
+    }
+    if (totalQuestions == '') {
+        alert('Please select an amount of questions');
+        return bool = true;
+
+    }
+    if (bool === false) {
+        //alert('traemos las preguntas de la api');
+        const url = `https://opentdb.com/api.php?amount=${totalQuestions}&category=${category}&difficulty=${difficult}`;
+
+        fetch(url)
+            //devolver la respues en formato json
+            .then(response => response.json())
+            .then((data) => printData(data.results));
+    }
+    console.log(bool);
+}
+
+function printCategory(categoryData) {
+    const container = document.getElementById('category');
+
+    categoryData.forEach(element => {
+        container.innerHTML += `<option value="${element.id}">${element.name}</option>`;
+    });
+}
+
+
+function printData(data) {
+    const container = document.getElementById('questions-container');
+    let html = '';
+
+    for (let i = 0; i < data.length; i++) {
+        questions.push({ question: data[i].question, answer: data[i].correct_answer });
+        data[i].incorrect_answers.push(data[i].correct_answer);
+        if (data[i].incorrect_answers.length > 2) {
+            html += `<h5 id="question"}">${i+1}.-${data[i].question}</h5>
+                <select id="answer-${i}"  class="form-select" aria-label="Default select example">
+                    <option selected value=""></option>
+                    <option value= "${data[i].incorrect_answers[0]}">${data[i].incorrect_answers[0]}</option>
+                    <option value= "${data[i].incorrect_answers[1]}">${data[i].incorrect_answers[1]}</option>
+                    <option value= "${data[i].incorrect_answers[2]}">${data[i].incorrect_answers[2]}</option>
+                    <option value= "${data[i].incorrect_answers[3]}">${data[i].incorrect_answers[3]}</option>
+                </select>
+                `;
+        } else {
+            html += `<h5 id="question"}">${i+1}.-${data[i].question}</h5>
+                <select id="answer-${i}" class="form-select" aria-label="Default select example">
+                    <option selected value=""></option>
+                    <option value= "${data[i].incorrect_answers[0]}">${data[i].incorrect_answers[0]}</option>
+                    <option value= "${data[i].incorrect_answers[1]}">${data[i].incorrect_answers[1]}</option>
+                </select>`;
+
+        }
+    }
+    html += `<button onclick="checkResult()" type="submit" class="btn btn-primary">Send Answers</button>`
+    container.innerHTML = html;
+}
+
+function checkResult() {
+    let score = 0;
+    const container = document.getElementById('finalScore');
+    for (let i = 0; i < questions.length; i++) {
+        const result = document.getElementById(`answer-${i}`).value;
+        console.log(result);
+        if (result == '') {
+            alert('Please choose your answer to question: ' + (i + 1));
+            document.getElementById(`answer-${i}`).focus;
+
+        } else {
+            if (result === questions[i].answer) {
+                score += 1;
+
+            }
+        }
+
+    }
+    container.innerHTML = `<div class="score" id="finalScore">
+                                <p class=final>Your Score is ${score} of ${questions.length}</p>
+                                <button type="submit" onclick="#" class="btn btn-primary">Try Again</button>
+                           </div>`;
+
+
+
+}
+
+
+getCategory();
